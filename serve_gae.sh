bd5e34d00c71138ab634a3204d66faeab7376a76
Alexander Trakhimenok <533159+trakhimenok@users.noreply.github.com>
2024-10-09 08:31:05 +0100

    fix: serve_gae.sh cleanup

--- serve_gae.sh
+++ serve_gae.sh
@@ -1,15 +1,16 @@
-echo "Starting local GAE server..."
 export GCLOUD_PROJECT="demo-local-sneat-app"
 export FIREBASE_AUTH_EMULATOR_HOST="localhost:9099"
 export FIRESTORE_EMULATOR_HOST="localhost:8080"
 export SNEAT_TG_DEV_BOTS="AlextDevBot:sneat_bot"
 #export GOOGLE_APPLICATION_CREDENTIALS="/Users/alexandertrakhimenok/projects/sneat/sneat-team-go/private_keys/demo-sneat.json"
 
-script_dirname=$(dirname "$(realpath "$0")")
-#script_parent_dir=$(dirname "$(dirname "$(realpath "$0")")")
-go_main_path="$script_dirname"
-go run "$go_main_path"
-#
-#SCRIPT=$(realpath "$0")
-#SCRIPT_PATH=$(dirname "$SCRIPT")
-#go run "$SCRIPT_PATH"/../src/sneatgaemain --with-bots
+SCRIPT_DIR=$(dirname "$(readlink -f "$0" || realpath "$0")")
+
+echo "Started serve_gae.sh:, SCRIPT_DIR=$SCRIPT_DIR"
+cd "$SCRIPT_DIR" || exit
+
+echo "Running 'go mod tidy'..."
+go mod tidy
+
+echo "Starting local GAE server..."
+go run .
